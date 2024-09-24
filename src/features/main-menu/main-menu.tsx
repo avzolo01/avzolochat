bf5e3fec31b4a1fbc954005353b0b6e401f4f2dd
avzolo01 <ivci@gobiit.com>
2024-09-24 12:58:39 -0400

    Update main-menu.tsx

--- src/features/main-menu/main-menu.tsx
+++ src/features/main-menu/main-menu.tsx
@@ -6,21 +6,14 @@ import {
   MenuItemContainer,
   menuIconProps,
 } from "@/ui/menu";
-import {
-  Book,
-  Home,
-  MessageCircle,
-  PocketKnife,
-  Sheet,
-  VenetianMask,
-} from "lucide-react";
-import { getCurrentUser } from "../auth-page/helpers";
+import { Book, Home, MessageCircle, PocketKnife, VenetianMask } from "lucide-react";
+import { getCurrentUser } from "../auth-page/helpers"; // Importing user auth
 import { MenuLink } from "./menu-link";
 import { UserProfile } from "./user-profile";
 
 export const MainMenu = async () => {
-  const user = await getCurrentUser();
-
+  const user = await getCurrentUser(); // Fetch the current user
+  
   return (
     <Menu>
       <MenuBar>
@@ -32,42 +25,37 @@ export const MainMenu = async () => {
           </MenuItem>
           <MenuTrayToggle />
         </MenuItemContainer>
-        <MenuItemContainer>
-          <MenuItem tooltip="Chat">
-            <MenuLink href="/chat" ariaLabel="Go to the Chat page">
-              <MessageCircle {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="Persona">
-            <MenuLink href="/persona" ariaLabel="Go to the Persona configuration page">
-              <VenetianMask {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="extensions">
-            <MenuLink href="/extensions" ariaLabel="Go to the Extensions configuration page">
-              <PocketKnife {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          <MenuItem tooltip="prompts">
-            <MenuLink href="/prompt" ariaLabel="Go to the Prompt Library configuration page">
-              <Book {...menuIconProps} />
-            </MenuLink>
-          </MenuItem>
-          {user.isAdmin && (
-            <>
-              <MenuItem tooltip="reporting">
-                <MenuLink href="/reporting" ariaLabel="Go to the Admin reporting" >
-                  <Sheet {...menuIconProps} />
+        
+        {/* Conditionally render 'personas' and 'extensions' for admin users */}
+        {user.isAdmin && (
+          <>
+            <MenuItemContainer>
+              <MenuItem tooltip="Personas" asChild>
+                <MenuLink href="/persona" ariaLabel="Go to the Personas page">
+                  <VenetianMask {...menuIconProps} />
                 </MenuLink>
               </MenuItem>
-            </>
-          )}
-        </MenuItemContainer>
+            </MenuItemContainer>
+            
+            <MenuItemContainer>
+              <MenuItem tooltip="Extensions" asChild>
+                <MenuLink href="/extensions" ariaLabel="Go to the Extensions page">
+                  <PocketKnife {...menuIconProps} />
+                </MenuLink>
+              </MenuItem>
+            </MenuItemContainer>
+          </>
+        )}
+
         <MenuItemContainer>
-          <MenuItem tooltip="Profile">
-            <UserProfile />
+          <MenuItem tooltip="Settings" asChild>
+            <MenuLink href="/settings" ariaLabel="Go to the Settings page">
+              <Sheet {...menuIconProps} />
+            </MenuLink>
           </MenuItem>
         </MenuItemContainer>
+        
+        <UserProfile />
       </MenuBar>
     </Menu>
   );
